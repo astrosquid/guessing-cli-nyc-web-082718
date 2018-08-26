3cb900cadb610b6dc313ee7a944a41caa4c262d1
Chris Bojemski <chris.bojemski@gmail.com>
2018-08-26 19:20:10 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,1 +1,12 @@
-# Code your solution here!
+def run_guessing_game
+  loop do
+    target = 1 + Random.rand(6)
+    input = gets.chomp
+    if input == "exit" break
+    elsif input.to_i == target 
+      puts "You guessed the correct number!"
+    else 
+      puts "The computer guessed #{target}."
+    end 
+  end 
+end
